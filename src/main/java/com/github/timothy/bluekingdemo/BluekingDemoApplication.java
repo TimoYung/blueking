238621e316c8f5677f7dcf82ae14c1770c3d6bf7
p_yzzhiyang <p_yzzhiyang@tencent.com>
2021-07-30 22:40:31 +0800

    init

---
+++ src/main/java/com/github/timothy/bluekingdemo/BluekingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.timothy.bluekingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluekingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluekingDemoApplication.class, args);
+    }
+
+}
